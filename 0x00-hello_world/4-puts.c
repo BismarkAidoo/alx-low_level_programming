b827ccbedb3e1eba2d7448dbb447962d1268ecb1
Bismark <mc07.aidoo@gmail.com>
2023-03-16 22:21:46 +0000

    Coding in C, so cool

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main -Print a line of code using puts function
+ *
+ * return:(0)
+*/
+int imain(void)
+{
+	puts("\"Programming is like buildng a multilingual puzzle"\n);
+	return (0);
+}
