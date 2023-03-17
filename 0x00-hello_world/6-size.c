fb8f1f992e281a1bdd80843af740449babf7bdc3
Bismark <mc07.aidoo@gmail.com>
2023-03-17 01:38:44 +0000

    Coding in C, so cool

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
 /**
- * main -
+ * main - Print the size of various data type in C
  *
- * return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+	char u;
+	int v;
+	long int w;
+	long long int x;
+	float y;
+
+	printf("Size of a char: %c byte(s)\n", (unsigned char)sizeof(u));
+	printf("Size of int: %u byte(s)\n", (unsigned int)sizeof(v));
+	printf("Size 0f long int: %lu byte(s)\n", (unsigned long)sizeof(w));
+	printf("Size of long long int: %llu byte(s)\n", (unsigned long long)sizeof(x));
+	printf("Size of float: %f byte(s)\n", (float)sizeof(y));
+	return (0);
 
 }
