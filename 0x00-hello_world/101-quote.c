08585c53fd08cb984f0841cb2fa0f342b3789b59
Bismark <mc07.aidoo@gmail.com>
2023-03-17 03:01:32 +0000

    Coding in C, so cool

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print a line of code to the standard error
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful"\"
+			- Dora Korpa, 2015-10-19\");
+	return (1);
+}
