6a1354108996d5f37139afe34f8e50ac39e7073a
Bismark <mc07.aidoo@gmail.com>
2023-03-22 05:50:04 +0000

    Looping through C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 /**
  * times_table - a function that prints 9 times table,
  * starting with 0.
- * Return: Always 0
+ * Return: empty output
  */
 
 void times_table(void)
@@ -26,14 +26,16 @@ void times_table(void)
 				_putchar(d + '0');
 				_putchar(u + '0');
 			}
-			else if (y != 0)
+			else
 			{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-			}
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
 
-			_putchar(z + '0');
+				_putchar(z + '0');
 			}
 		}
 
