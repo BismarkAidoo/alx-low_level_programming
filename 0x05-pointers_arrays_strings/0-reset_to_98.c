6eca53e85a9e7d76efbb8a30f1b0c967a39d65bf
Bismark <mc07.aidoo@gmail.com>
2023-03-27 20:41:44 +0000

    Mapping up

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,12 @@
 
 /**
  * reset_to_98 - Takes a pointer to an int and updates
- *               the value it points to to 98.
- * @n: The pointer to an int.
+ *               the value it points to 98
+ * @n: parameter to update  
+ * Return: Always 0
  */
 
 void reset_to_98(int *n)
 {
-
 	*n = 98;
-
 }
